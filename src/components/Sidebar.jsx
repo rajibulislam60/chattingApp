816734f23ed7d8c8a220da4ba67588c129a8dbe7
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-08-16 22:13:21 +0600

    sidebar done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ProfileImage from "../assets/profile.jpg";
 import { IoHomeOutline } from "react-icons/io5";
+import { AiFillMessage } from "react-icons/ai";
+import { FaRegBell } from "react-icons/fa";
+import { GoGear } from "react-icons/go";
+import { ImExit } from "react-icons/im";
 
 const Sidebar = () => {
   return (
@@ -13,9 +17,25 @@ const Sidebar = () => {
             alt="profile Image"
           />
         </div>
-        <div className="w-full h-[88px] relative ">
-          <div className="w-[161px] h-[88px] bg-white ml-auto flex items-center"></div>
+        <div className="w-full h-[88px] relative mt-[78px] ">
+          <div className="w-[161px] h-[88px] bg-white ml-auto flex items-center rounded-s-[20px] after:w-[8px] after:h-full after:top-0 after:right-0 after:absolute after:bg-primary after:rounded-s-[20px] after:shadow-red-900 after:shadow-2xl"></div>
           <IoHomeOutline className="text-[46px] w-full mx-auto absolute top-2/4 translate-y-[-50%] text-primary" />
+        </div>
+        <div className="w-full h-[88px] relative mt-[57px] ">
+          <div className="hidden w-[161px] h-[88px] bg-white ml-auto items-center rounded-s-[20px] after:w-[8px] after:h-full after:top-0 after:right-0 after:absolute after:bg-primary after:rounded-s-[20px] after:shadow-red-900 after:shadow-2xl"></div>
+          <AiFillMessage className="text-[46px] w-full mx-auto absolute top-2/4 translate-y-[-50%] text-slate-200" />
+        </div>
+        <div className="w-full h-[88px] relative mt-[57px] ">
+          <div className="hidden w-[161px] h-[88px] bg-white ml-auto items-center rounded-s-[20px] after:w-[8px] after:h-full after:top-0 after:right-0 after:absolute after:bg-primary after:rounded-s-[20px] after:shadow-red-900 after:shadow-2xl"></div>
+          <FaRegBell className="text-[46px] w-full mx-auto absolute top-2/4 translate-y-[-50%] text-slate-200" />
+        </div>
+        <div className="w-full h-[88px] relative mt-[57px] ">
+          <div className="hidden w-[161px] h-[88px] bg-white ml-auto items-center rounded-s-[20px] after:w-[8px] after:h-full after:top-0 after:right-0 after:absolute after:bg-primary after:rounded-s-[20px] after:shadow-red-900 after:shadow-2xl"></div>
+          <GoGear className="text-[46px] w-full mx-auto absolute top-2/4 translate-y-[-50%] text-slate-200" />
+        </div>
+        <div className="w-full h-[88px] relative mt-[187px] ">
+          <div className="hidden w-[161px] h-[88px] bg-white ml-auto items-center rounded-s-[20px] after:w-[8px] after:h-full after:top-0 after:right-0 after:absolute after:bg-primary after:rounded-s-[20px] after:shadow-red-900 after:shadow-2xl"></div>
+          <ImExit className="text-[46px] w-full mx-auto absolute top-2/4 translate-y-[-50%] text-white" />
         </div>
       </div>
     </div>
